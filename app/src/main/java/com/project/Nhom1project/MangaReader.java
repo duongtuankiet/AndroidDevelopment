9a79208f51b1b7e2f236ded88865f86cdedcf6c5
Dương Tuấn Kiệt <dtuankiet2802@gmail.com>
2022-11-29 18:46:37 +0700

    update

--- app/src/main/java/com/project/Nhom1project/MangaReader.java
+++ app/src/main/java/com/project/Nhom1project/MangaReader.java
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MangaReader extends AppCompatActivity {
     ImageView imageView;
     ImageButton imageButton;
+    Button prev;
+    Button next;
     @SuppressLint("MissingInflatedId") @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +23,17 @@ public class MangaReader extends AppCompatActivity {
         assert actionBar != null;
         actionBar.hide();
         setContentView(R.layout.activity_manga_reader);
+        prev = findViewById(R.id.b1);
+        next = findViewById(R.id.b3);
+        prev.setOnClickListener(v-> Toast.makeText(MangaReader.this,"Tính năng đang phát triển",Toast.LENGTH_LONG).show());
+        next.setOnClickListener(v-> Toast.makeText(MangaReader.this,"Tính năng đang phát triển",Toast.LENGTH_LONG).show());
+        int img = getIntent().getIntExtra("img",R.drawable.logo);
+        String s = getResources().getResourceEntryName(img);
+        s = s+"_raw";
+        int picid = getResources().getIdentifier(s,"drawable",getPackageName());
         imageButton =findViewById(R.id.imageButton);
         imageView = findViewById(R.id.imageManga);
-        imageView.setImageResource(R.drawable.isekaiojisan_raw);
+        imageView.setImageResource(picid);
         imageButton.setOnClickListener(v -> {
             Intent i = new Intent(MangaReader.this,MainActivity.class);
             startActivity(i);
